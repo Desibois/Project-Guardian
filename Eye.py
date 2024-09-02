6bd93ec45fecfd7ea90685b7672d2269343ee3cc
Desibois <145998810+Desibois@users.noreply.github.com>
2024-09-02 11:59:44 +0000

    Update Eye.py

--- Eye.py
+++ Eye.py
@@ -1,59 +1,48 @@
 import numpy as np
 import cv2
 
+xlimit = 1.8
+ylimit = 1.8
+
 cap = cv2.VideoCapture(0)
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
-coordinates = [0, 0]
+coordinates = [0, 0, 0, 0]
 
+def face_centre(coordinates):
+    x = coordinates[0]
+    y = coordinates[1]
+    w = coordinates[2]
+    h = coordinates[3]
+    centre_x = x + (w//2)
+    centre_y = y + (h//2)
+    return [centre_x, centre_y]
+    
+def calculate_move(face, centre):
+    if face[0] > centre[0] * xlimit:
+    	print('move left')
+    elif face[0] < centre[0] * xlimit:
+        print('move right')
+    if face[1] > centre[1] * ylimit:
+        print('move down')
+    elif face[1] < centre[1] * ylimit:
+        print('move up')
+
+        
+        
 while True:
     ret, frame = cap.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     height, width = gray.shape
-
-    max_height = 6/10 * height
-    min_height = 4/10 * height
-    max_width = 6/10 * width
-    min_width = 4/10 * width
-
+    centre = [width//2, height//2]
+    
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-        
     if len(faces) > 0:
-        print("IN FRAME")
-        for x, y, w, h in faces:
-            coordinates[0] = x
-            coordinates[1] = y          
+        print('in frame')
+        coordinates = faces[0]
     elif coordinates[0] == 0 and coordinates[1] == 0:
         pass
-
-    elif coordinates[0] < min_width:
-        print("Move left")
-        coordinates = [0, 0]
-
-    elif coordinates[0] > max_width:
-        print("Move right")
-        coordinates = [0, 0]
-    
-    if len(faces) > 0:
-        print("IN FRAME")
-        for x, y, w, h in faces:
-            coordinates[0] = x
-            coordinates[1] = y  
-                    
-    elif coordinates[0] == 0 and coordinates[1] == 0:
-        pass
-
-    elif coordinates[1] < min_height:
-        print("Move down")
-        coordinates = [0, 0]
-        
-    elif coordinates[1] > max_height:
-        print("Move up")
-        coordinates = [0, 0]
-    
-
-    cv2.imshow('frame', frame)
-    if cv2.waitKey(1) == ord("j"):
-        break
-
-cap.release()
+    else:
+        face = face_centre(coordinates)
+        calculate_move(face, centre)
+        face = [0, 0, 0, 0]
